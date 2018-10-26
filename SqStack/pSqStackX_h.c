365150de061575a27758cc6792ad40803648e18b
PinkHat <2641797006@qq.com>
2018-10-26 22:24:11 +0800

    -

--- SqStack/pSqStackX_h.c
+++ SqStack/pSqStackX_h.c
@@ -1,25 +1,48 @@
 int InitStack();
-void push(value);
-void pop(address);
+int DestroyStack();
+int push(value);
+int pop(address);
 
 int InitStack();
-/*	调用且仅调用一次此函数
+/*	请调用且仅调用一次此函数
 	此函数初始化栈
 	成功返回0, 失败返回-1
 */
 
-void push(value);
-/*	将value压栈,
-	value:
-		value可以是立即数, char,int,double类型值或指针, 也可以是一个结构体
-	push失败将设置全局变量 push_err为-1, (push_err初始值为0)
-	push成功不会设置push_err的值
+int DestroyStack();
+/*	此函数销毁栈
+	销毁后 如要使用栈, 请使用InitStack()初始化栈
+	可以不使用此函数
+	如非必要 请不要使用此函数
 */
 
-void pop(address);
-/*	根据提供的地址出栈, 注意与push对应
-	pop失败将设置全局变量 pop_err为-1, (pop_err初始值为0)
-	pop成功不会设置pop_err的值
+int push(value);
+/*	将value压栈,
+	value:
+		value可以是立即数, char,int,double类型值或指针, 也可以是一个任意大小的结构体
+	成功返回0, 失败返回-1
+	一般不会出错, (进行大量压栈时可能因为申请内存失败而失败)
+	一般情况可以不检查错误
+*/
+
+int pop(address);
+/*	根据提供的地址address进行出栈, 
+	注意:
+		pop应与push对应
+	成功返回0, 失败返回-1
+	一般不会出错,
+	(当栈内没有数据, 或者数据大小不够address类型时不会进行出栈操作, 此时返回-1)
+	一般情况可以不检查错误
 */
 
 //如果发现任何错误, 可以点击页面右上方联系我们
+
+
+
+
+
+
+
+
+
+

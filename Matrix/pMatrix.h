867d91fa5790417188a3d7b624a40fb260b8ebbe
PinkHat <2641797006@qq.com>
2018-11-07 11:31:04 +0800

    -

--- Matrix/pMatrix.h
+++ Matrix/pMatrix.h
@@ -13,19 +13,19 @@ int __MATRIX__;
 typedef struct{
 	int row;
 	int col;
-	float** bp;
+	double** bp;
 }MATRIX;
 
 int InitMatrix(MATRIX* mat, int row, int col)
 {
 	int i;
-	float** bp;
-	bp=(float**)malloc(row*sizeof(float*));
+	double** bp;
+	bp=(double**)malloc(row*sizeof(double*));
 	if(!bp)
 		return -1;
 	mat->bp=bp;
 	for(i=0;i<row;i++){
-		*(bp+i)=(float*)malloc(col*sizeof(float));
+		*(bp+i)=(double*)malloc(col*sizeof(double));
 		if(!*(bp+i))
 			return i+1;
 	}
@@ -37,7 +37,7 @@ int InitMatrix(MATRIX* mat, int row, int col)
 void DestroyMatrix(MATRIX* mat)
 {
 	int i, row;
-	float** bp;
+	double** bp;
 	row=mat->row;
 	bp=mat->bp;
 	for(i=0;i<row;i++)
@@ -48,7 +48,7 @@ void DestroyMatrix(MATRIX* mat)
 void PrintMatrix(MATRIX* mat)
 {
 	int i, j, row, col;
-	float **bp, *p;
+	double **bp, *p;
 	bp=mat->bp;
 	row=mat->row;
 	col=mat->col;
@@ -60,10 +60,10 @@ void PrintMatrix(MATRIX* mat)
 	}
 }
 
-int MatrixTraverse(MATRIX* mat, int (*visit)(float*, int, int))
+int MatrixTraverse(MATRIX* mat, int (*visit)(double*, int, int))
 {
 	int i, row, ret;
-	float **bp;
+	double **bp;
 	bp=mat->bp;
 	row=mat->row;
 	for(i=0;i<row;i++){
@@ -84,16 +84,57 @@ int InverseNum(int* a, int n)
 	return num;
 }
 
-float DetValue(MATRIX* mat)
+#ifndef _PDETVALUEX_
+double DetValue(MATRIX* mat)
 {
-	float** bp=mat->bp;
+	double** bp=mat->bp;
 	double det=0, product;
 	int order=mat->row;
 	if(mat->col!=order){
 		__MATRIX__=-1;
 		return 0;
 	}
-	int arr[order+2], *a=arr+1, *t=a+order-1, *p, *q, i, x=0x2, y=0, z;
+	int arr[order+2], *a=arr+1, *t=a+order-1, *p, *q, *r, *s, i, x=0x2, w=1, v=1;
+	for(p=a;p<=t;p++)
+		*p=p-a;
+	arr[0]=-1;
+	a[order]=-1;
+	do{
+		for(p=a,product=1,i=0; p<=t; p++,i++)
+			product*=*(*(bp+i)+*p);
+		p=t, w=1;
+		while(*p<*--p)
+			w++;
+		if(x&=3, (x+=1)&2)
+			det+=product;
+		else
+			det-=product;
+		(w-v-1)/2%2==1 ? ( x=((x&2)==0 ? 2 : 0)) : 0;
+		if(w>=order)
+			break;
+		v=w, q=p;
+		while(*++q>*p);
+		i=*--q, *q=*p, *p=i;
+		p++, q=t;
+		while(p<q)
+			i=*p, *p++=*q, *q--=i;
+	}while(1);
+	__MATRIX__=0;
+	return det;
+}
+
+#else
+
+double DetValue(MATRIX* mat)
+{
+	double** bp=mat->bp;
+	double det=0, product;
+	int order=mat->row;
+	if(mat->col!=order){
+		__MATRIX__=-1;
+		return 0;
+	}
+	int arr[order+2], *a=arr+1, *t=a+order-1, *p, *q, i;
 	for(p=a;p<=t;p++)
 		*p=p-a;
 	arr[0]=-1;
@@ -110,24 +151,20 @@ float DetValue(MATRIX* mat)
 		p++, q=t;
 		while(p<q)
 			i=*p, *p++=*q, *q--=i;
+		return 0;
 	}
 	do{
 		for(p=a,product=1,i=0; p<=t; p++,i++)
 			product*=*(*(bp+i)+*p);
-#ifndef _PQUICK_
 		if(InverseNum(a, order)%2==0)
-#else
-		if(z=(x+=1)&0x2, y++, y>=0x18 ? (x = (y=0, x>0x19) ? 0 : 2) : 0, z)
-#endif
 			det+=product;
 		else
 			det-=product;
 	}while(NextOrdinal()>=0);
 	__MATRIX__=0;
-	return (float)det;
+	return det;
 }
-
-
+#endif
 
 
 
